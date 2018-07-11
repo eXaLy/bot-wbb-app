d20525590b3060cbf058446c3df81c2a90c106d7
Aswin Ly <aswin.ly@gmail.com>
2018-07-11 22:05:58 +0200

    Fix config settings

--- src/configuration/configHandler.ts
+++ src/configuration/configHandler.ts
@@ -1,6 +1,12 @@
+import * as dotenv from 'dotenv';
+
 export class ConfigHandler {
 
+  constructor() {
+    dotenv.config();
+  }
+
   public getBotToken() : string {
-    return process.env.npm_package_config_bottoken;
+    return process.env.BOT_TOKEN;
   }
 }
